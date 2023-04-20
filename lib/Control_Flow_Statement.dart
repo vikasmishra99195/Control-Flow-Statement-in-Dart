c7f2e4b2351e257fbd05a08cff9b47e31f9f5def
Vikas Mishra <vikasmishra99195@gmail.com>
2023-04-20 17:58:18 +0530

    Control Flow Statement

---
+++ lib/Control_Flow_Statement.dart
@@ -0,0 +1,74 @@
+void main() {
+  // # Control Flow Statement
+
+  // ## If & Else Statement
+  //   Syntax:
+
+  // if(Condition){
+  //   Statement
+  // }
+  // else{
+  //   Statement
+  //   }
+
+  // Example If Statement
+
+  // bool is_login = true;
+  // if(is_login){
+  //   print("Hello Developer");
+  // }
+
+  // bool is_login1 = true;
+  // if(is_login1==true){
+  //   print("Hello Developer");
+  // }
+
+  // bool is_login2 = false;
+  // if(is_login2==false){
+  //   print("Hello Developer");
+  // }
+
+  // var a = 10;
+  // if(a==10){
+  //   print("Hello Developer");
+  // }
+  //
+  // var a1 = 20;
+  // if(a==10){
+  //   print("Hello Developer");
+  // }
+
+  // ## If Else Statement
+
+  // var is_login = true;
+  // if(is_login){
+  //   print("Hello Developer");
+  // } else{
+  //   print("Please login first!");
+  // }
+
+  // var is_login1 = false;
+  // if(is_login1){
+  //   print("Hello Developer");
+  // } else{
+  //   print("Please login first!");
+  // }
+
+  // var a = 10;
+  // if(a==10){
+  //   print("Given number is 10");
+  // } else{
+  //   print("Given no is not 10");
+  // }
+
+  // var a1 = 20;
+  // if(a1==10){
+  //   print("Given number is 10");
+  // } else{
+  //   print("Given no is not 10");
+  // }
+
+
+
+
+}
